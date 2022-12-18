5cd92cc4be69e0a5a99503e0cb65b770cd154aba
dimnissv <dmitrijdenisov359@gmail.com>
2022-12-18 19:56:40 +0600

    dev

--- Site/urls.py
+++ Site/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 from Site import settings
 
@@ -12,8 +13,10 @@ urlpatterns = [
     path('video/', include('video.urls')),
     path('search/', include('search.urls')),
     path('oreder_table/', include('order_table.urls')),
-    path('kodik/', include('kodik.urls'))
+    path('kodik/', include('kodik.urls')),
+    path('97905f0ebd3eb55b94a0e70660b9059d.txt/', lambda r: HttpResponse('97905f0ebd3eb55b94a0e70660b9059d'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+    
